cd4cc1c26fd589959713ba3d314f54faedbe7337
Migliatti <gabimpariz@icloud.com>
2025-11-17 16:12:33 -0300

    Create rock_paper_scsissor.py: Implement initial structure for rock, paper, scissors game with rules and user instructions.

---
+++ sixth_excercise/rock_paper_scsissor.py
@@ -0,0 +1,10 @@
+# Lucas wants to create a rock, paper, and scissors game to play against the computer. He needs a program that allows the user to choose an option and then displays the result of the match.
+
+# Create a program that allows the user to choose between rock, paper, or scissors. The computer will randomly choose an option. The program should display who won the match. Remember that:
+
+#     Rock beats Scissors (Rock crushes Scissors);
+#     Scissors beats Paper (Scissors cuts Paper);
+#     Paper beats Rock (Paper covers Rock);
+#     If both choose the same option, it's a tie.
+
+
