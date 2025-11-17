424cd928efbe0d2ffd57e817c623b3ab37fa7447
Migliatti <gabimpariz@icloud.com>
2025-11-17 14:44:03 -0300

    Create validate_cpf.py

---
+++ second_excercise/validate_cpf.py
@@ -0,0 +1,3 @@
+# Carlos works in a court and needs to validate if the CPF entered by the client is in the correct format before proceeding with the service. The CPF must contain exactly 11 numeric digits. If the entry contains letters or any other character that is not a number, the program must display an error message.
+
+# Create a program that asks the user for a CPF number and verifies if it has 11 digits and contains only numbers.
