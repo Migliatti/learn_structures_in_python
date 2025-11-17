a08e8807d8bd01e8931543427bce3de8e9fc6579
Migliatti <gabimpariz@icloud.com>
2025-11-17 15:40:17 -0300

    Create large_words.py: Implement functionality to identify and highlight words longer than 10 letters in a given text.

---
+++ fourth_excercise/large_words.py
@@ -0,0 +1,3 @@
+# Sofia is a text reviewer and needs to identify very long words in a paragraph. Easier-to-read texts tend to have short words, so she wants a program that finds words longer than 10 letters and highlights them.
+
+# Create a program that receives a text and displays all words that have more than 10 letters. If no long word is found, the program should inform the user.
